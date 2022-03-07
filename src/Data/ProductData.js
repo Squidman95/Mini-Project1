49ef0cf4d8ab3ee1eb0decbce84da0978f0f6e25
scpe <scpe@itu.dk>
2022-03-07 11:00:53 +0100

    Add products

--- src/Data/ProductData.js
+++ src/Data/ProductData.js
@@ -3,24 +3,170 @@ export const products = [
         name: 'Dog bed',
         id: '0',
         price: 100,
+        animal: 'Dog',
         category: 'Accessories',
         subCategory: 'Bed',
-        image: 'https://m.media-amazon.com/images/I/81amc1cWs0L._AC_SL1500_.jpg'
+        image: 'https://m.media-amazon.com/images/I/81amc1cWs0L._AC_SL1500_.jpg',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
     },
     {
         name: 'Dog food - dry',
         id: '1',
         price: 70,
+        animal: 'Dog',
         category: 'Food',
         subCategory: 'Dry food',
-        image: 'https://m.media-amazon.com/images/I/81753eskvxL._AC_SX466_.jpg'
+        image: 'https://m.media-amazon.com/images/I/81753eskvxL._AC_SX466_.jpg',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
     },
     {
         name: 'Dog collar - Red',
         id: '2',
         price: 120,
+        animal: 'Dog',
         category: 'Accessory',
         subCategory: 'Collar',
-        image: 'https://www.dfordog.co.uk/user/products/red-dingo-dog-collar-red.jpg'
+        image: 'https://www.dfordog.co.uk/user/products/red-dingo-dog-collar-red.jpg',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+    },
+    {
+        name: 'Dog food - wet',
+        id: '3',
+        price: 370,
+        animal: 'Dog',
+        category: 'Food',
+        subCategory: 'Wet food',
+        image: '/src/assets/imgages/dog/dog_food_wet.jpg',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+    },
+    {
+        name: 'Dog food - glutenfree',
+        id: '4',
+        price: 470,
+        animal: 'Dog',
+        category: 'Food',
+        subCategory: 'Glutenfree food',
+        image: '/src/assets/imgages/dog/dog_food_glutenfree.jpg',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+    },
+    {
+        name: 'Dog toy - squeaker',
+        id: '5',
+        price: 70,
+        animal: 'Dog',
+        category: 'Toy',
+        subCategory: 'Squeaker',
+        image: '/src/assets/imgages/dog/dog_seahorse_squeaker.jpg',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+    },
+    {
+        name: 'Cat Bed',
+        id: '6',
+        price: 150,
+        animal: 'Cat',
+        category: 'Accessory',
+        subCategory: 'Bed',
+        image: '/src/assets/imgages/cat/cat_bed.jpg',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+    },
+    {
+        name: 'Cat food - dry',
+        id: '7',
+        price: 140,
+        animal: 'Cat',
+        category: 'Food',
+        subCategory: 'Dry',
+        image: '/src/assets/imgages/cat/cat_food_dry.jpg',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+    },
+    {
+        name: 'Cat food - wet',
+        id: '8',
+        price: 130,
+        animal: 'Cat',
+        category: 'Food',
+        subCategory: 'wet',
+        image: '/src/assets/imgages/cat/cat_food_wet.jpg',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+    },
+    {
+        name: 'Cat toy - snack mouse',
+        id: '8',
+        price: 30,
+        animal: 'Cat',
+        category: 'Toy',
+        subCategory: 'Intelligence',
+        image: '/src/assets/imgages/cat/cat_toy_snackMouse.jpg',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+    },
+    {
+        name: 'Bird Cage',
+        id: '9',
+        price: 250,
+        animal: 'Bird',
+        category: 'Accessory',
+        subCategory: 'Cage',
+        image: '/src/assets/imgages/bird/bird_cage1.jpg',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+    },
+    {
+        name: 'Bird Cage',
+        id: '10',
+        price: 200,
+        animal: 'Bird',
+        category: 'Accessory',
+        subCategory: 'Cage',
+        image: '/src/assets/imgages/bird/bird_cage2.jpg',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+    },
+    {
+        name: 'Bird Food',
+        id: '11',
+        price: 110,
+        animal: 'Bird',
+        category: 'Food',
+        subCategory: 'Food',
+        image: '/src/assets/imgages/bird/bird_food.jpg',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+    },
+    {
+        name: 'Rodent Cage',
+        id: '12',
+        price: 140,
+        animal: 'Rodent',
+        category: 'Accessory',
+        subCategory: 'Cage',
+        image: '/src/assets/imgages/rodent/rodent_cage.jpg',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+    },
+    {
+        name: 'Rodent Food',
+        id: '13',
+        price: 100,
+        animal: 'Rodent',
+        category: 'Food',
+        subCategory: 'Food',
+        image: '/src/assets/imgages/rodent/rodent_food.jpg',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+    },
+    {
+        name: 'Rodent Bed',
+        id: '14',
+        price: 40,
+        animal: 'Rodent',
+        category: 'Accessory',
+        subCategory: 'Bed',
+        image: '/src/assets/imgages/rodent/rodent_bed.jpg',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+    },
+    {
+        name: 'Rodent Toy - running wheel',
+        id: '15',
+        price: 70,
+        animal: 'Rodent',
+        category: 'Toy',
+        subCategory: 'Running Wheel',
+        image: '/src/assets/imgages/rodent/rodent_toy_runningWheel.jpg',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
     },
 ]
